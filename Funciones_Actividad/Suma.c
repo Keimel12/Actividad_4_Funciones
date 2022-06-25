d37c92971c16cf7a0da98ee45da42ac0499d9f45
Keimel R <keimelrodriguez12@gmail.com>
2022-06-25 19:02:45 -0430

    Cambio de nombre para que sea mas sencillo orientarse entre los codigos

---
+++ Funciones_Actividad/Suma.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*  Escribir una función que sume dos números. Implemente un programa
+que la utilice */
+
+int suma(int x, int y){
+    int sum = x + y;
+    return sum;
+}
+
+int main()
+{
+    int n1;
+    int n2;
+
+    printf("Dime el primer numero a sumar: ");
+    scanf("%d", &n1);
+    printf("Dime el segundo numero a sumar: ");
+    scanf("%d", &n2);
+    printf("La suma total da igual a: %d", suma(n1, n2));
+    return 0;
+}
---
+++ Funciones_Actividad/media.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/* Ejercicio 2. Escribir un programa que reciba como argumento un conjunto de
+números. Calcular la media mediante una función */
+int main()
+{
+
+}
