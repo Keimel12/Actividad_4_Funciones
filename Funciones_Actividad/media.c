05e8c3d59d57ee977235c9033d8a6cf75aa5c829
Keimel R <keimelrodriguez12@gmail.com>
2022-06-25 20:56:09 -0430

    Cambiamos las variables de las lineas 13 y 16 a un tipo float y devolvemos a la variable cant en la linea 15 como un int, añadimos al terminar el bucle for la variable i con el valor de cant, agregamos los & en los scanf y ahora nos permite obtener una media con decimales

--- Funciones_Actividad/media.c
+++ Funciones_Actividad/media.c
@@ -4,25 +4,26 @@ números. Calcular la media mediante una función */
 
 float media(float a, int b){
     float prom;
-    prom = a/b;
+    prom = (a/b);
     return prom;
 }
 
 int main()
 {
-    int n;
+    float n;
     int i;
-    float cant;
-    int sum = 0;
+    int cant;
+    float sum = 0;
 
     printf("Dime la cantidad de numeros: ");
-    scanf("%d", cant);
+    scanf("%d", &cant);
     for(i = 1; i <= cant; i++)
     {
         printf("%d- Dime el numero: ", i);
-        scanf("%f", n);
+        scanf("%f", &n);
         sum += n;
     }
+    i = cant;
     printf("El resultado de la media aritemtica es; %.1f", media(sum, i));
     return 0;
 }
