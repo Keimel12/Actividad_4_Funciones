4aa2917f7eedab31e77f4c892631fe107609f82d
Keimel R <keimelrodriguez12@gmail.com>
2022-06-26 14:20:29 -0430

    Incluimos la libreira stdlib.h y creamos codigo para obtener las primera cadenas de caracteres

--- Funciones_Actividad/concatenar.c
+++ Funciones_Actividad/concatenar.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /* Ejercicio 3. Escribir un programa que reciba una serie de cadenas de caracteres
 como argumentos. El programa deberá concatenarlas y dar la vuelta a la cadena
@@ -8,5 +9,11 @@ utilizar la memoria justa y necesaria. */
 
 int main()
 {
+    char cadena1[51]:
+    char cadena2[51]:
 
+    printf("Primera cadena de caracteres: ");
+    scanf("%[^\n]", cadena1);
+    printf("Segunda cadena de caracteres: ");
+    scanf("%[^\n]", cadena2);
 }
