690b8244586f53fb434758975adb51ab83b0f485
Keimel R <keimelrodriguez12@gmail.com>
2022-06-25 23:41:34 -0430

    Inicio de la actividad 3

---
+++ Funciones_Actividad/concatenar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+/* Ejercicio 3. Escribir un programa que reciba una serie de cadenas de caracteres
+como argumentos. El programa deberá concatenarlas y dar la vuelta a la cadena
+resultante. La cadena que almacene la concatenación de los argumentos debe
+utilizar la memoria justa y necesaria. */
+
+int main()
+{
+
+}
