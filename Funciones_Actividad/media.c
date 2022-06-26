d9b44b43bb4e7f42575bbed890999a47adaf1689
Keimel R <keimelrodriguez12@gmail.com>
2022-06-25 20:11:32 -0430

    Establecemos la funcion del promedio

--- Funciones_Actividad/media.c
+++ Funciones_Actividad/media.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 /* Ejercicio 2. Escribir un programa que reciba como argumento un conjunto de
 números. Calcular la media mediante una función */
+
+float (int a, int b){
+    prom = a/b;
+    return prom;
+}
+
 int main()
 {
 
