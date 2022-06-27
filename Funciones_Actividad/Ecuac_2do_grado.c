648f8ebe75909620c86e1b91e7148d79bfe6408b
Keimel R <keimelrodriguez12@gmail.com>
2022-06-26 20:10:04 -0430

    Declaramos las variables, la funcion y permitimos al usuario poder teclear las variables a, b y c

--- Funciones_Actividad/Ecuac_2do_grado.c
+++ Funciones_Actividad/Ecuac_2do_grado.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 /* Ejercicio 4. Escribir un programa que calcule y presente las raices reales de la ecuacion de 
 segundo grado.
 ax² + bx + c = 0.
@@ -6,15 +7,24 @@ Suponer que a,b y c son argumentos de coma flotante con valores dados y que x¹ 
 en coma flotante. Suponer tambien que b > 4*a*c, de forma que las raices calculadas son siempre 
 reales. */
 
-float variables(float x1, float x2){
-    
+float variables(float x1, float x2) // Parametros
+{
+
+
 }
 
 int main()
 {
+    //Argumentos
     float a;
     float b;
     float c;
 
+    printf("Dime el valor que sera asignado a <a>: ")
+    scanf("%f", &a);
+    printf("Dime el valor que sera asignado a <b>: ")
+    scanf("%f", &b);
+    printf("Dime el valor que sera asignado a <c>: ")
+    scanf("%f", &c);
     return 0;
 }
