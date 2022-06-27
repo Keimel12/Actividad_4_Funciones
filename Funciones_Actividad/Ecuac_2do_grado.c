80170c5024b1b6f6056affdd44def1749535cabb
Keimel R <keimelrodriguez12@gmail.com>
2022-06-26 19:45:05 -0430

    Iniciamos con el codigo que calculara las raices reales de la ecuacion de segundo grado

---
+++ Funciones_Actividad/Ecuac_2do_grado.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* Ejercicio 4. Escribir un programa qie calcule y presente las raicesreales de la ecuacion de 
+segundo grado.
+ax² + bx + c = 0.
+Suponer que a,b y c son argumentos de coma flotante con valores dados y que x¹ y que x² son variables
+en coma flotante. Suponer tambien que b > 4*a*c, de forma que las raices calculadas son siempre 
+reales. */
+int main()
+{
+
+    return 0;
+}
