75f1cdef401c490df1e7536d2b1c157da1caf221
Keimel R <keimelrodriguez12@gmail.com>
2022-06-27 15:12:20 -0430

    Creamos codigo dentro de las funciones para que puedan calcular las raices enteras (falta que pueda calcular una sola raiz si se da el caso) y omita el calculo de raices imaginarias y creamos un do while para que el usuario pueda decidir si quiere acabar el programa

--- Funciones_Actividad/Ecuac_2do_grado.c
+++ Funciones_Actividad/Ecuac_2do_grado.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 /* Ejercicio 4. Escribir un programa que calcule y presente las raices reales de la ecuacion de 
 segundo grado.
 ax² + bx + c = 0.
@@ -10,17 +11,34 @@ reales. */
 float var1(float a, float b, float c) // Parametros
 {
     float x1;
+    float cont = pow(b, 2) - 4*a*c;
 
-    x1 = ((-b) + sqrt(pow(b, 2) - 4*a*c)) / (2*a);
-    return x1;
+    if ((a == 0) || (cont < 0)){
+        printf("\nNo se puede resolver");
+        return 0;
+    }
+
+    if(cont > 0){
+        x1 = (-b + sqrt(cont)) / (2*a);
+        return x1;
+    }
 }
 
-float var2(float b, float a, float c) // Parametros
+float var2(float a, float b, float c) // Parametros
 {
     float x2;
+    float cont = pow(b, 2) - (4*a*c);
 
+    if ((a == 0) || (cont < 0)){
+        printf("\nNo se puede resolver");
+        return 0;
+    }
 
-
+    if(cont > 0){
+        x2 = ((-b) - sqrt(cont)) / (2*a);
+        return x2;
+    }
+    
 }
 
 int main()
@@ -29,15 +47,25 @@ int main()
     float a;
     float b;
     float c;
+    char option;
 
+    do
+    {
     printf("Dime el valor que sera asignado a <a>: ");
     scanf("%f", &a);
     printf("Dime el valor que sera asignado a <b>: ");
     scanf("%f", &b);
     printf("Dime el valor que sera asignado a <c>: ");
     scanf("%f", &c);
-    printf("El resultado de x¹ es: %.2f", var1(a, b, c));
-   // printf("El resultado de x² es: %.2f", var)
 
+    printf("\nEl resultado de x1 es: %.2f", var1(a, b, c));
+    printf("\nEl resultado de x2 es: %.2f", var2(a, b, c));
+
+    getchar();
+    printf("\nQuieres continuar? Presione <s> para continuar: ");
+    scanf("%c", &option);
+    system("cls");
+    } while (option == 's' || option == 'S');
+    printf("Programa finalizado!!");
     return 0;
 }
