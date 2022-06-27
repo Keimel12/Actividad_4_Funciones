65f1dcccdcff254f080fe77b5aa897cc0bfd0779
Keimel R <keimelrodriguez12@gmail.com>
2022-06-26 19:50:20 -0430

    Establecemos los parametros y argumentos

--- Funciones_Actividad/Ecuac_2do_grado.c
+++ Funciones_Actividad/Ecuac_2do_grado.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
-/* Ejercicio 4. Escribir un programa qie calcule y presente las raicesreales de la ecuacion de 
+/* Ejercicio 4. Escribir un programa que calcule y presente las raices reales de la ecuacion de 
 segundo grado.
 ax² + bx + c = 0.
 Suponer que a,b y c son argumentos de coma flotante con valores dados y que x¹ y que x² son variables
 en coma flotante. Suponer tambien que b > 4*a*c, de forma que las raices calculadas son siempre 
 reales. */
+
+float variables(float x1, float x2){
+    
+}
+
 int main()
 {
+    float a;
+    float b;
+    float c;
 
     return 0;
 }
