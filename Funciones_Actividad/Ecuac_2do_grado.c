f37d1afe61393e55caf2bf52e829b6c06404826d
Keimel R <keimelrodriguez12@gmail.com>
2022-06-27 12:04:15 -0430

    Creamos una funcion llamada var1 que nos devolvera el valor de x1, establecemos las base para la funcion var2 que nos devolvera el valor de x2 y creamos un printf que nos demostrara el resultado de la primera

--- Funciones_Actividad/Ecuac_2do_grado.c
+++ Funciones_Actividad/Ecuac_2do_grado.c
@@ -7,8 +7,18 @@ Suponer que a,b y c son argumentos de coma flotante con valores dados y que x¹ 
 en coma flotante. Suponer tambien que b > 4*a*c, de forma que las raices calculadas son siempre 
 reales. */
 
-float variables(float x1, float x2) // Parametros
+float var1(float a, float b, float c) // Parametros
 {
+    float x1;
+
+    x1 = ((-b) + sqrt(pow(b, 2) - 4*a*c)) / (2*a);
+    return x1;
+}
+
+float var2(float b, float a, float c) // Parametros
+{
+    float x2;
+
 
 
 }
@@ -20,11 +30,14 @@ int main()
     float b;
     float c;
 
-    printf("Dime el valor que sera asignado a <a>: ")
+    printf("Dime el valor que sera asignado a <a>: ");
     scanf("%f", &a);
-    printf("Dime el valor que sera asignado a <b>: ")
+    printf("Dime el valor que sera asignado a <b>: ");
     scanf("%f", &b);
-    printf("Dime el valor que sera asignado a <c>: ")
+    printf("Dime el valor que sera asignado a <c>: ");
     scanf("%f", &c);
+    printf("El resultado de x¹ es: %.2f", var1(a, b, c));
+   // printf("El resultado de x² es: %.2f", var)
+
     return 0;
 }
